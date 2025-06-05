6810a2a454d81373c77764b866bc93d6a9958c4c
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-05 16:36:46 +0530

    Update Day6.cpp

--- DSA Progress/Week1/Day6.cpp
+++ DSA Progress/Week1/Day6.cpp
@@ -9,6 +9,7 @@ public:
         }
     }
 };
+// this question can also done by swaping last and start untile start<=last
 
 // 242. Valid Anagram
 class Solution {
@@ -51,4 +52,143 @@ public:
        return pre;
       
         }
+
+// i face problem in compare the char of first string with other string  at same time 
+
+
+// 75. Sort Colors
+// we can not use sort function of algorithm
+// because of that i use bubble sort
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=0;j<nums.size()-1-i;j++){
+                if(nums[j]>=nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i]<<" ";
+        }
+    }
+};
+
+
+// 169. Majority Element
+
+// brute force
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majSize=nums.size()/2;
+        int ind;
+        for(int i=0;i<nums.size();i++){
+            int count=1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count>majSize){
+                ind=i;
+            }
+        }
+        return nums[ind];
+    }
+}
+
+// issue is that with large input it give invalid ans
+
+// optimized
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+   int majSize=nums.size()/2;
+        sort(nums.begin(),nums.end());
+        int count=1;
+        for(int i=1;i<nums.size();i++){
+           if(nums[i]==nums[i-1]){
+            count++;
+           }
+           else{
+            count=1;
+           }
+
+           if(count>majSize){
+           return nums[i];
+           }
+          
+        }
+// 
+
+
+// 189. Rotate Array
+// brute force
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+
+      int st=0;
+    while(k--){
+        int nu=nums[nums.size()-1];
+        for(int i=nums.size()-1;i>0;i--){
+            nums[i]=nums[i-1];
+        }
+        nums[0]=nu;
+    
+    }
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+        
+    }
+};
+// time  exceede for large input
+
+// optimized
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+
+      reverse(nums.begin(),nums.end());
+        // reverse all element
+      reverse(nums.begin(),nums.begin()+k);
+        // reverse the k element
+       reverse(nums.begin()+k,nums.end());
+    // reverse other
+    
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+        
+    }
+};
+
+
+// 88. Merge Sorted Array
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+     
+      
+      for(int i=0;i<n;i++){
+       nums1[m+i]=nums2[i];
+      }
+      sort(nums1.begin(),nums1.end());
+      for(int i=0;i<nums1.size();i++){
+        cout<<nums1[i]<<" ";
+      }
+
+        
+    }
+};
+
+// in nums1 vector already space given for nums2 element as zero we have to put nums2 element in place of zero then sort
+// and this thing i missed after sometime i relised
 
