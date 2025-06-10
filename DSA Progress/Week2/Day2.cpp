3e1d8ce3a093c3fef26fe626ff59c6ee45f00cf5
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-10 19:58:42 +0530

    Update Day2.cpp

--- DSA Progress/Week2/Day2.cpp
+++ DSA Progress/Week2/Day2.cpp
@@ -515,3 +515,50 @@ int main(){
     ll.print();
 }
 
+
+
+
+// 83. Remove Duplicates from Sorted List
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    ListNode* deleteDuplicates(ListNode* head) {
+
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode * pre=head;
+        ListNode * curr=head->next;
+        while(curr!=nullptr){
+             if(pre->val==curr->val){
+                ListNode * temp=curr;
+                
+                pre->next=curr->next;
+                 curr=curr->next;
+                delete temp;
+               
+
+             }
+             else{
+                pre=pre->next;
+                curr=curr->next;
+             }
+           
+        }
+        return head;
+
+    }
+};
