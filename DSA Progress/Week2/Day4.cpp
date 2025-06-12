dea031f90cffabcfa5e6e71ed6875eb5465fcb1e
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-12 13:59:10 +0530

    Create Day4.cpp

---
+++ DSA Progress/Week2/Day4.cpp
@@ -0,0 +1,23 @@
+// / STL Conatainer
+// #include<list>
+// it's header file for linklist
+
+// push_back() it is function for push the element at the end
+// push_front() it is function for push the element at the front
+// pop_back() it is use to remove the element from back (it remove the last element)
+// pop_front() is pop the element from the front
+
+// size()  it give total no of element
+
+// Function	                                  Description
+// insert(position, value)	           Insert element at given iterator position
+// erase(position)	                     Remove element at given iterator position
+// reverse()	                              Reverse the list
+// sort()	                            Sort the list in ascending order
+// empty()	                             Returns true if the list is empty
+// begin()	                                Iterator to first element
+// end()	                             Iterator to past-the-end elemen
+// front()                             Value of first element (modifiable)
+// back()		                         Value of last element (modifiable)
+
+
