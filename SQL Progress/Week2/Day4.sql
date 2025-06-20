cf9821bbc14e31fa7cdae47a337ff8c67919dca6
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-20 10:40:30 +0530

    Update Day4.sql

--- SQL Progress/Week2/Day4.sql
+++ SQL Progress/Week2/Day4.sql
@@ -1,1 +1,95 @@
+-- Table: Cinema
 
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row contains information about the name of a movie, its genre, and its rating.
+-- rating is a 2 decimal places float in the range [0, 10]
+ 
+
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+
+-- Return the result table ordered by rating in descending order.
+
+
+select id,movie , description,rating 
+  from Cinema 
+  where id%2<>0 and description<>'boring'
+  order by rating desc ;
+
+
+
+-- Table: Prices
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+ 
+-- Table: UnitsSold
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- This table may contain duplicate rows.
+-- Each row of this table indicates the date, units, and product_id of each product sold. 
+ 
+-- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
+-- Return the result table in any order.
+
+
+select p.product_id,
+  case 
+  when sum(u.units)=0 then 0 when sum(u.units) is null then 0 else round((sum(u.units*p.price)/sum(u.units)) ,2) end as average_price
+from Prices as p
+  left join UnitsSold as u 
+  on p.product_id=u.product_id  and u.purchase_date between p.start_date and  p.end_date  
+  group by  p.product_id;
+
+
+
+-- Table: Project
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | project_id  | int     |
+-- | employee_id | int     |
+-- +-------------+---------+
+-- (project_id, employee_id) is the primary key of this table.
+-- employee_id is a foreign key to Employee table.
+-- Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+-- Table: Employee
+-- +------------------+---------+
+-- | Column Name      | Type    |
+-- +------------------+---------+
+-- | employee_id      | int     |
+-- | name             | varchar |
+-- | experience_years | int     |
+-- +------------------+---------+
+-- employee_id is the primary key of this table. It's guaranteed that experience_years is not NULL.
+-- Each row of this table contains information about one employee.
+--  Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+select  p.project_id ,round(sum(e.experience_years)/count(*),2) as average_years 
+ from Project as p 
+right join 
+Employee as e on 
+p.employee_id =e.employee_id group by p.project_id
+  having p.project_id is not null;
