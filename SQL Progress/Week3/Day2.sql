5b05bcb8e8d6492d56700708200c72847c97a4c9
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-24 10:08:25 +0530

    Create Day2.sql

---
+++ SQL Progress/Week3/Day2.sql
@@ -0,0 +1,44 @@
+-- Table: Employee
+-- +---------------+---------+
+-- | Column Name   |  Type   |
+-- +---------------+---------+
+-- | employee_id   | int     |
+-- | department_id | int     |
+-- | primary_flag  | varchar |
+-- +---------------+---------+
+-- (employee_id, department_id) is the primary key (combination of columns with unique values) for this table.
+-- employee_id is the id of the employee.
+-- department_id is the id of the department to which the employee belongs.
+-- primary_flag is an ENUM (category) of type ('Y', 'N'). If the flag is 'Y', the department is the primary department for the employee. If the flag is 'N', the department is not the primary.
+
+-- Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+
+with dept_count as (select  employee_id , count(department_id) as count_no  from 
+ Employee  group by employee_id )
+--  it create tempory table  (Common Table Expression (CTE))
+
+ select  e.employee_id , e.department_id  from Employee as e inner join dept_count as d
+ on d.employee_id=e.employee_id 
+ where (d.count_no=1) or (d.count_no>1 and e.primary_flag='Y')
+
+
+-- Table: Triangle
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | x           | int  |
+-- | y           | int  |
+-- | z           | int  |
+-- +-------------+------+
+-- In SQL, (x, y, z) is the primary key column for this table.
+-- Each row of this table contains the lengths of three line segments.
+
+-- Report for every three line segments whether they can form a triangle.
+
+select x,y,z ,case
+  when x+y>z and   x+z>y and  y+z>x
+  then 'Yes'
+  else 'No'
+  end as triangle 
+  from Triangle;
+
