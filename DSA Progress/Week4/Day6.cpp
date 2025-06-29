3bb42f6cfc66af4e1bbc5a0ce06d0899e8c3d6ff
Kundan Ray <149966879+raykundan655@users.noreply.github.com>
2025-06-29 18:14:30 +0530

    Update Day6.cpp

--- DSA Progress/Week4/Day6.cpp
+++ DSA Progress/Week4/Day6.cpp
@@ -1,1 +1,16 @@
+// 1295. Find Numbers with Even Number of Digits
 
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            string str=to_string(nums[i]);
+            if(str.size()%2==0){
+                count++;
+            }
+        }
+        return count;
+       
+    }
+};
